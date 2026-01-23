ea4e179e64628100a3b665386ea5af1914f9f20c
Lishavadivelmurgan <LISHAMURGAN2106@GMAIL.COM>
2026-01-23 12:49:16 +0530

    Add files via upload

---
+++ Models/product.cs
@@ -0,0 +1,13 @@
+﻿namespace Razor.Models
+{
+    public class product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public int Quanity { get; set; }
+
+    }
+}
